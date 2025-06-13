af37909b9ff9b14b037c7b25224d264b74ea1ba3
Gethray99 <98901059+Gethray99@users.noreply.github.com>
2025-06-13 14:37:07 +0530

    completed working

--- pdf_Q&A.py
+++ pdf_Q&A.py
@@ -2,14 +2,20 @@ import os
 from dotenv import load_dotenv
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_gemini import Gemini
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
-pdf_loader = PyPDFLoader("/CV/Gethwan_Suriyawansa.pdf")
+pdf_loader = PyPDFLoader("CV/Yasan_Punchihewa_CV.pdf")
 
 documents = pdf_loader.load()
 
-chain = load_qa_chain(llm=Gemini(model="gemini-1.5-flash", api_key=api_key, verbose=True))
-
+chain = load_qa_chain(llm=ChatGoogleGenerativeAI(model="models/gemini-1.5-flash", api_key=api_key, verbose=True), chain_type="stuff")
+query = "what is the email of the person?"
+response = chain.invoke({
+    "input_documents": documents,
+    "question": query
+})
+print(f"Response: {response['output_text']}")
